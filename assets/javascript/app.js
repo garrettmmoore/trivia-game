22627073e14a395760015370ad85a6b1d2ab3b50
Garrett Moore <garrettmarkmoore@gmail.com>
2017-08-06 21:38:03 -0700

    fixed solution and answer matching

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,13 @@
 
 $( document ).ready(function() {
+$("#wrong-answer-text-box").hide();
 
+var unanswered = 0;
 var countDownInterval=null;
 // Create a timer and pass these two values
 function timer()
 {
-	var counter = 30;
+	var counter = 20;
 	$("#time-box").html(counter);
 	countDownInterval = setInterval(function() 
 	{
@@ -20,6 +22,7 @@ function timer()
 		{
         	clearInterval(countDownInterval);
         	answerHelper("djhfsdla", questions[index-1]);
+        	unanswered++;
 
 		}
     
@@ -28,22 +31,28 @@ function timer()
 
 // Declare Global Variables
 
-var wins = 1;
-var loss = 1;
+var wins = 0;
+var loss = 0;
 var count = 0;
+
 
 	// Create a start function to begin game on click
 	function startGame(){
 		// Start button for user to click
 		$("#button1").click(function(){
+			clearInterval(countDownInterval);
+			index = 0;
 			// Increase count so that first question can be called
 			count++;
+
+			// Hide button after click
+			$(this).hide();	
+			
 			// Generates new question on click
 			newQuestion();
 			// Start Timer
 			
-			// Hide button after click
-			$(this).hide();	
+			
 		});
 	}
 	// Call startGame function to get everything started on click
@@ -62,14 +71,14 @@ var count = 0;
 	}
 
 	// Create new object for each question
-	var q1 = new Trivia("What year was nintendo founded?", ["1950", "1889", "1982", "1970"], "1889", "<img src='assets/images/mario.png'/>", "mario rocks");
-	var q2 = new Trivia("What was the first Pokemon game released in Japan?", ["Red", "Blue", "Yellow", "Green"], "Green", "<img src='assets/images/pikachu.png'/>", "Pikachu rocks");
-	var q3 = new Trivia("Which of these titles was Luigi's first game?", ["Mario 64", "Luigi's Dream Castle", "Luigi and Peach", "Luigi's Mansion"], "Luigi's Mansion", "<img src='assets/images/luigi.png'/>", "luigi rocks");
-	var q4 = new Trivia("What color did Miyamoto originally want Kirby to be?", ["Red", "White", "Yellow", "Pink"], "Yellow", "<img src='assets/images/kirby.png'/>", "kirby rocks");
-	var q5 = new Trivia("Which of these characters didn't speak until Super Mario Sunshine?", ["Yoshi", "Toad", "Bowser", "Mario"], "Bowser", "<img src='assets/images/bowser.png'/>", "bowser rocks");
-	var q6 = new Trivia("Which of these characters was the first villain in Mario?", ["Bowser", "Yoshi", "Donkey Kong", "Ganondorf"], "Donkey Kong", "<img src='assets/images/donkey-kong.png'/>", "donkey kong rocks");
-	var q7 = new Trivia("Which of these titles was released on the Nintendo Gamecube for The Legend of Zelda?", ["Majora's Mask", "Ocarina of Time", "Wind Waker", "A Link to the Past"], "Wind Waker", "<img src='assets/images/toon-link.png'/>", "toon link rocks");
-	var q8 = new Trivia("The character Samus is a ?", ["Man", "Alien", "Unknown", "Woman"], "Woman", "<img src='assets/images/samus.png'/>", "samus rocks");
+	var q1 = new Trivia("What year was nintendo founded?", ["1950", "1889", "1982", "1970"], "1889", "<img src='assets/images/mario.png'/>", "The correct answer was 1889.");
+	var q2 = new Trivia("What was the first Pokemon game released in Japan?", ["Red", "Blue", "Yellow", "Green"], "Green", "<img src='assets/images/pikachu.png'/>", "The correct answer was Green.");
+	var q3 = new Trivia("Which of these titles was Luigi's first game?", ["Mario 64", "Luigi's Dream Castle", "Luigi and Peach", "Luigi's Mansion"], "Luigi's Mansion", "<img src='assets/images/luigi.png'/>", "The correct answer was Luigi's Mansion.");
+	var q4 = new Trivia("What color did Miyamoto originally want Kirby to be?", ["Red", "White", "Yellow", "Pink"], "Yellow", "<img src='assets/images/kirby.png'/>", "The correct answer was Yellow.");
+	var q5 = new Trivia("Which of these characters didn't speak until Super Mario Sunshine?", ["Yoshi", "Toad", "Mario", "Bowser"], "Bowser", "<img src='assets/images/bowser.png'/>", "The correct answer was Bowser.");
+	var q6 = new Trivia("Which of these characters was the first villain in Mario?", ["Bowser", "Yoshi", "Donkey Kong", "Ganondorf"], "Donkey Kong", "<img src='assets/images/donkey-kong.png'/>", "The correct answer was Donkey Kong.");
+	var q7 = new Trivia("Which of these titles was released on the Nintendo Gamecube for The Legend of Zelda?", ["Majora's Mask", "Ocarina of Time", "Wind Waker", "A Link to the Past"], "Wind Waker", "<img src='assets/images/toon-link.png'/>", "The correct answer was Wind Waker.");
+	var q8 = new Trivia("The character Samus is a ?", ["Man", "Alien", "Unknown", "Woman"], "Woman", "<img src='assets/images/samus.png'/>", "The correct answer was Woman.");
 
 
 	var questions = [q1, q2, q3, q4, q5, q6, q7, q8];
@@ -94,22 +103,101 @@ var count = 0;
 
 						// Question 1
 						var question = questions[index];
-						var choicesHtml = $(".choices")
+						console.log("index: " + index);
+						var choicesHtml = $(".choices");
 						for(var i = 0; i < choicesHtml.length; i++){
 
 							var choiceHTML = $(choicesHtml[i]);
 							var selection = questions[index].choices[i];
 							$(choiceHTML).html(selection);
 
-							choiceHTML.off("click");
-							choiceHTML.on("click", function()
+							// choiceHTML.off("click");
+
+
+							// $("#choice-one-box.choices").on("click", function()
+							// {
+							// 	// count++;
+							// 	var userSelection = questions[index-1].choices[0];
+							// 	answerHelper(userSelection, question);
+								
+							// 	clearInterval(countDownInterval);
+								
+							// });
+
+							// $("#choice-two-box.choices").on("click", function()
+							// {
+							// 	// count++;
+							// 	var userSelection = questions[index-1].choices[1];
+							// 	answerHelper(userSelection, question);
+								
+							// 	clearInterval(countDownInterval);
+								
+							// });
+
+							// $("#choice-three-box.choices").on("click", function()
+							// {
+							// 	// count++;
+							// 	var userSelection = questions[index-1].choices[2];
+							// 	answerHelper(userSelection, question);
+								
+							// 	clearInterval(countDownInterval);
+								
+							// });
+
+							// $("#choice-four-box.choices").on("click", function()
+							// {
+							// 	// count++;
+							// 	var userSelection = questions[index-1].choices[3];
+							// 	answerHelper(userSelection, question);
+								
+							// 	clearInterval(countDownInterval);
+								
+							// });
+
+
+						}
+							$(".choices").off("click");
+
+
+							$("#choice-one-box.choices").on("click", function()
 							{
 								// count++;
-								answerHelper(selection, question);
+								var userSelection = questions[index-1].choices[0];
+								answerHelper(userSelection, question);
 								
 								clearInterval(countDownInterval);
+								
 							});
-						}
+
+							$("#choice-two-box.choices").on("click", function()
+							{
+								// count++;
+								var userSelection = questions[index-1].choices[1];
+								answerHelper(userSelection, question);
+								
+								clearInterval(countDownInterval);
+								
+							});
+
+							$("#choice-three-box.choices").on("click", function()
+							{
+								// count++;
+								var userSelection = questions[index-1].choices[2];
+								answerHelper(userSelection, question);
+								
+								clearInterval(countDownInterval);
+								
+							});
+
+							$("#choice-four-box.choices").on("click", function()
+							{
+								// count++;
+								var userSelection = questions[index-1].choices[3];
+								answerHelper(userSelection, question);
+								
+								clearInterval(countDownInterval);
+								
+							});
 
 			index++;
 		}
@@ -118,13 +206,13 @@ var count = 0;
 
 	
 
-	function answerHelper(choice, question)
+	function answerHelper(userSelection, question)
 	{
-		    if(choice.includes(question.correctAnswer))
+		    if(userSelection.includes(question.correctAnswer))
 		    {
 		    	// alert("Yay it worked!!");
 
-		    	$("#correct-question-box").html("Winner correct question box..Winner");
+		    	$("#correct-question-box").html("Correct!");
 		    	$(".question-choices-box").hide();
 		    	$("#generate-image-box").html(question.correctImage);
 		    	// $("#wrong-answer-text-box").html(questionDotWrongAnswerText);
@@ -138,13 +226,13 @@ var count = 0;
 		    	    	// $("#wrong-answer-text-box").hide();
 		    	    	$(".question-choices-box").show();
 		    	    	newQuestion();
-				  	}, 5000);	
+				  	}, 3500);	
 				  		// newQuestion();
 		    }
 		    else
 		    {
 		    	// alert("sorry!!");
-		    	$("#correct-question-box").html("lost correct question box..lost");
+		    	$("#correct-question-box").html("Incorrect..sorry.");
 		    	$(".question-choices-box").hide();
 		    	$("#generate-image-box").html(question.correctImage);
 		    	$("#wrong-answer-text-box").html(question.wrongAnswerText);
@@ -158,7 +246,7 @@ var count = 0;
 		    	    	$("#wrong-answer-text-box").hide();
 		    	    	$(".question-choices-box").show();
 		    	    	newQuestion();
-				  	}, 5000);
+				  	}, 3500);
 		    		// newQuestion(count++);
 		    }
 		    count++;
@@ -177,16 +265,16 @@ function endGame(){
 		$("#button1").show();
 		$("#choice-one-box").html("Correct Answers: " + wins);
 		$("#choice-two-box").html("Inorrect Answers: " + loss);
-		$("#choice-three-box").html("Unanswered: ");
+		$("#choice-three-box").html("Unanswered: " + unanswered);
 		$("#choice-four-box").html("Click Start to play again!");
 		count = 0;
 		win = 0;
 		loss = 0;
+		unanswered = 0;
 		index = 0;
 		clearInterval(countDownInterval);
+		console.log(index);
 		startGame();
-
-
 }
 
 
